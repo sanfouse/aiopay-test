807a6a55490f6f92d7b004117f5ab97d99543cf0
sanfouse <gfgfuyyy@gmail.com>
2022-06-28 17:28:13 +0300

    fix

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ app.on_startup.append(create)
 
 if __name__ == '__main__':
     aiohttp_jinja2.setup(app,
-        loader=jinja2.FileSystemLoader(str(BASE_DIR / 'aiohttpdemo_polls' / 'templates')))
+        loader=jinja2.FileSystemLoader(str(BASE_DIR / 'aiopay-test' / 'templates')))
     setup(app,
         EncryptedCookieStorage(b'Thirty  two  length  bytes  key.'))    
     setup_routes(app)
--- models/__init__.py
+++ models/__init__.py
@@ -5,7 +5,7 @@ db = Gino()
 PG_URL = "postgresql://{user}:{password}@{host}:{port}/{database}".format(
     host="localhost",
     port=5432,
-    user='physics',
+    user='postgres',
     password=15610348,
     database="postgres",
 )
